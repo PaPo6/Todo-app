228a257d4c26c7c0e2fdecdad212acce376ba0d3
PaPo6 <54620344+PaPo6@users.noreply.github.com>
2021-06-21 18:55:34 +0200

    Add switcher and add clear completed

--- index.js
+++ index.js
@@ -4,78 +4,153 @@
 let input = document.querySelector('#add');
 let btn = document.querySelector('.btnCheck');
 let toDoList = document.querySelector('.todo-list');
-let li = document.getElementsByTagName('.todo-list li');
-let check = document.querySelector('.co')
+let checkbox = document.querySelector('.checkbox');
+let check = document.querySelector('.check');
+let label = document.querySelector('label');
+let itemsLeft = document.querySelector('.items-left span');
+let completedFilterButton = document.querySelector('#completed');
+let switcher = document.querySelector('.switcher');
 
-let toDoListLenght = toDoList.lenght;
+itemsLeft.innerText = document.querySelectorAll('.todo-list li label input:checked').length;
 
-let listLenght = li.lenght;
+let todos = [];
 
-// const CHECK = "fas fa-circle co";
-// const UNCHECK = "far fa-circle co";
-// const LINE_THROUGHT = "lineThrough";
+
+
+
+
 
 const addElement = function () {
+    // let li = document.querySelector('ul');
+
+    
     let txt = document.getElementById("add").value;
     if(txt === ""){
         alert("you must write something");
     } else {
-        toDoList.innerHTML += `<li>
-        <i class ="far fa-circle co" action ="complete"></i>
-        <img src="images/icon-check.svg" alt="" class = "check">
+        const li = document.createElement('li');
+        li.innerHTML += `
+        <label>
+        <input type="checkbox">
+        <div class="checkbox">
+          <div class="check">
+          </div>
+        </div>
         <span>${txt}</span>
-        <img src="images/icon-cross.svg" alt="delete" class="delete"></li>`;
-    }
+        <img src="images/icon-cross.svg" alt="delete" class="delete">
+        </label>
+        `;
 
-};
+        toDoList.appendChild(li);
+    // if (document.querySelector('.filter input[type="checkbox"]:checked').id === "completed") {
+    //     li.classList.add('hidden');
+    
+    li.addEventListener("click", () => {
+        li.classList.toggle('checked');
+    })
+    };
+
+    updateItemsCount(1)
+   
+}
+btn.addEventListener("click", addElement);
+
+
+
+
+
 
 // const addElement = function () {
+//     // let li = document.querySelector('ul');
+
+    
 //     let txt = document.getElementById("add").value;
 //     if(txt === ""){
 //         alert("you must write something");
 //     } else {
 //         toDoList.innerHTML += `<li>
-//         <input type="checkbox" id="todo_${toDoListLenght}">
+//         <label>
+//         <input type="checkbox">
+//         <div class="checkbox">
+//           <div class="check">
+//           </div>
+//         </div>
 //         <span>${txt}</span>
-//         <img src="images/icon-cross.svg" alt="delete" class="delete"></li>`;
-//     }
+//         <img src="images/icon-cross.svg" alt="delete" class="delete">
+//         </label>
+//         </li>`;
+//     // if (document.querySelector('.filter input[type="checkbox"]:checked').id === "completed") {
+//     //     li.classList.add('hidden');
+    
+//     li.addEventListener("click", () => {
+//         li.classList.toggle('checked');
+//     })
+//     };
 
-// };
+//     updateItemsCount(1)
+   
+// }
+// btn.addEventListener("click", addElement);
 
-btn.addEventListener("click", addElement);
 
-const completeToDo = function(element){
-    const CHECK = "fa-check-circle";
-    const UNCHECK = "fa-circle";
-    element.classList.toggle(CHECK);
-    element.classList.toggle(UNCHECK);
-    element.parentNode.querySelector(".text").classList.toggle('.checkedLine');
+
+const updateItemsCount = function(number) {
+    itemsLeft.innerText = +itemsLeft.innerText + number;
 }
-
-input.addEventListener("keydown", (event) => {
-    if(event.keyCode == 13) {
+input.addEventListener('keypress', (event) => {
+    if(event.keyCode == 13 || event.key === "Enter" ) {
+    // todos.push( {value: event.target.value, checked: false});
     addElement()
+    // console.log(todos);
     }
 });
 
-// input.addEventListener("keyup", (event) => {
-//     if(event.keyCode === 13 || event.key === "Enter") {
-//     toDoList.push(event.target.value);
-//     newToDO(e.target.value);
-//     input.value = "";
-//     }
-// });
-
-//mark todos as complete
-// toDoList.addEventListener('click', e => {
-//     if(e.target.tagName == 'SPAN'){
-//         e.target.classList.toggle('completed');
-//     }
-// })
-function deleteToDoList(e){
-    if (e.target.classList.contains('delete')){
-        e.target.parentElement.remove();
-    }
+function deleteToDoList(li){
+    li.remove();
+    updateItemsCount(-1);
+    
 }
-toDoList.addEventListener('click',deleteToDoList);
 
+
+toDoList.addEventListener('click', (f) => {
+    if (f.target.classList.contains('delete')){
+        deleteToDoList(f.target.parentElement);
+    }
+});
+
+//CLEAR all
+document.querySelector('.clearAll').addEventListener('click', () => {
+    document.querySelectorAll('.todo-list li label input:checked + .checkbox').forEach(item => {
+    deleteToDoList(item.closest('li'));
+    });
+});
+
+
+
+//completed
+const completedElement = function(){
+    
+    if (toDoList.classList.contains('checked')) {
+        li.classList.remove('checked');
+    } 
+    else {
+        li.classList.add('checked');
+    }
+    
+}
+
+completedFilterButton.addEventListener("click", completedElement)
+
+document.querySelector('#completed').addEventListener('click', () => {
+    document.querySelectorAll('.todo-list li label input:not(:checked) + .checkbox').forEach(item => {
+    item.closest('li').classList.add('hidden');
+    });
+});
+
+
+
+//switcher
+const changeTheme = function(){
+    document.body.classList.toggle("light");
+}
+switcher.addEventListener("click", changeTheme)
