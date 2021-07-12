3e5eff3a65347475e329e815aa883694eef91724
PaPo6 <54620344+PaPo6@users.noreply.github.com>
2021-07-12 18:14:01 +0200

    Final project

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 //add element
 let input = document.querySelector('#add');
 let btn = document.querySelector('.btnCheck');
@@ -11,146 +9,108 @@ let itemsLeft = document.querySelector('.items-left span');
 let completedFilterButton = document.querySelector('#completed');
 let switcher = document.querySelector('.switcher');
 
-itemsLeft.innerText = document.querySelectorAll('.todo-list li label input:checked').length;
+itemsLeft.innerText = document.querySelectorAll('.dragula input').length;
 
 let todos = [];
 
-
-
-
-
-
-const addElement = function () {
-    // let li = document.querySelector('ul');
-
-    
-    let txt = document.getElementById("add").value;
-    if(txt === ""){
-        alert("you must write something");
-    } else {
-        const li = document.createElement('li');
-        li.innerHTML += `
-        <label>
-        <input type="checkbox">
-        <div class="checkbox">
-          <div class="check">
-          </div>
-        </div>
-        <span>${txt}</span>
-        <img src="images/icon-cross.svg" alt="delete" class="delete">
-        </label>
-        `;
-
-        toDoList.appendChild(li);
-    // if (document.querySelector('.filter input[type="checkbox"]:checked').id === "completed") {
-    //     li.classList.add('hidden');
-    
-    li.addEventListener("click", () => {
-        li.classList.toggle('checked');
-    })
-    };
-
-    updateItemsCount(1)
-   
+//switcher
+const changeTheme = function () {
+    document.body.classList.toggle("light");
 }
-btn.addEventListener("click", addElement);
+switcher.addEventListener("click", changeTheme);
 
 
-
-
-
-
-// const addElement = function () {
-//     // let li = document.querySelector('ul');
-
-    
-//     let txt = document.getElementById("add").value;
-//     if(txt === ""){
-//         alert("you must write something");
-//     } else {
-//         toDoList.innerHTML += `<li>
-//         <label>
-//         <input type="checkbox">
-//         <div class="checkbox">
-//           <div class="check">
-//           </div>
-//         </div>
-//         <span>${txt}</span>
-//         <img src="images/icon-cross.svg" alt="delete" class="delete">
-//         </label>
-//         </li>`;
-//     // if (document.querySelector('.filter input[type="checkbox"]:checked').id === "completed") {
-//     //     li.classList.add('hidden');
-    
-//     li.addEventListener("click", () => {
-//         li.classList.toggle('checked');
-//     })
-//     };
-
-//     updateItemsCount(1)
-   
-// }
-// btn.addEventListener("click", addElement);
-
-
-
-const updateItemsCount = function(number) {
-    itemsLeft.innerText = +itemsLeft.innerText + number;
-}
 input.addEventListener('keypress', (event) => {
-    if(event.keyCode == 13 || event.key === "Enter" ) {
-    // todos.push( {value: event.target.value, checked: false});
-    addElement()
-    // console.log(todos);
+    if (event.keyCode === 13 || event.key === "Enter") {
+        addElement(input.value);
+        input.value = '';
     }
 });
 
-function deleteToDoList(li){
-    li.remove();
-    updateItemsCount(-1);
-    
+//add item
+const addElement = function () {
+    // const elem = document.createElement('li');
+
+    let txt = document.getElementById("add").value;
+    if (txt === "") {
+        alert("you must write something");
+    } else {
+        const elem = document.createElement('label');
+        elem.classList.add('dragula');
+        elem.innerHTML = `
+            <input type="checkbox">
+            <div class="checkbox">
+            <div class="check">
+            </div>
+            </div>
+            <span>${txt}</span>
+            <img src="images/icon-cross.svg" alt="delete" class="delete">
+            `;
+
+        if (document.querySelector('.filters input:checked').id === 'completed') {
+            elem.classList.add('hidden');
+        }
+
+        toDoList.append(elem);
+        updateItemsCount(document.querySelectorAll('.dragula input').length);
+    }
+}
+btn.addEventListener("click", addElement);
+
+const updateItemsCount = function (number) {
+    itemsLeft.innerText = number;
 }
 
 
+//remove todo item
+function deleteToDoList(elem) {
+    elem.remove();
+    updateItemsCount(document.querySelectorAll('.dragula input').length);
+}
 toDoList.addEventListener('click', (f) => {
-    if (f.target.classList.contains('delete')){
+    if (f.target.classList.contains('delete')) {
         deleteToDoList(f.target.parentElement);
     }
 });
 
 //CLEAR all
 document.querySelector('.clearAll').addEventListener('click', () => {
-    document.querySelectorAll('.todo-list li label input:checked + .checkbox').forEach(item => {
-    deleteToDoList(item.closest('li'));
+    document.querySelectorAll('.dragula input:checked + .checkbox').forEach(item => {
+        deleteToDoList(item.closest('label'));
     });
 });
 
+//all filter
+document.querySelector('#all').addEventListener('click', () => {
+    const allFilter = document.querySelectorAll('.dragula input + .checkbox')
+    allFilter.forEach(item => {
+        item.closest('label').style.display = "grid";
+    });
+    updateItemsCount(allFilter.length);
+});
 
-
-//completed
-const completedElement = function(){
-    
-    if (toDoList.classList.contains('checked')) {
-        li.classList.remove('checked');
-    } 
-    else {
-        li.classList.add('checked');
-    }
-    
-}
-
-completedFilterButton.addEventListener("click", completedElement)
-
+//completed filter
 document.querySelector('#completed').addEventListener('click', () => {
-    document.querySelectorAll('.todo-list li label input:not(:checked) + .checkbox').forEach(item => {
-    item.closest('li').classList.add('hidden');
+    document.querySelectorAll('.dragula input:not(:checked) + .checkbox').forEach(item => {
+        item.closest('label').style.display = "none";
     });
+    const completedFilter = document.querySelectorAll('.dragula input:checked + .checkbox')
+    completedFilter.forEach(item => {
+        item.closest('label').style.display = "grid";
+    });
+    updateItemsCount(completedFilter.length);
+});
+//active filter
+document.querySelector('#active').addEventListener('click', () => {
+    document.querySelectorAll('.dragula input:checked + .checkbox').forEach(item => {
+        item.closest('label').style.display = "none";
+    });
+    const activeFilter = document.querySelectorAll('.dragula input:not(:checked) + .checkbox')
+    activeFilter.forEach(item => {
+        item.closest('label').style.display = "grid";
+    });
+    updateItemsCount(activeFilter.length);
 });
 
 
-
-//switcher
-const changeTheme = function(){
-    document.body.classList.toggle("light");
-}
-switcher.addEventListener("click", changeTheme)
+dragula([document.getElementById('sortable')]);
